43bd4cb228f18f5a4af65ccc4759fa2f7014d86c
sonu sharma <echosonusharma@gmail.com>
2025-07-20 18:59:34 +0000

    feat: add gh workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  base: '/zen-reader/',
   plugins: [solid()],
   worker: {
     format: 'es',
